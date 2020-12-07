44dbbd828904ea013c961451224de498fb9c81a3
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-12-07 11:56:57 -0800

    time struct

--- framework/src/app.rs
+++ framework/src/app.rs
@@ -1,11 +1,16 @@
 use crate::Frame;
 
 /// represents an application that can be run by the framework
-pub trait App
+pub trait App: 'static
 {
     /// the name of this app, which will be displayed as the window
     /// title
     const TITLE: &'static str = "Playground";
+
+    /// the default width(in pixels) for the window and framebuffer
+    const WIDTH: usize = 600;
+    /// the default height(in pixels) for the window and framebuffer
+    const HEIGHT: usize = 400;
 
     /// render to the window
     fn render(&self, frame: Frame);
--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,5 +1,104 @@
 mod frame;
+mod time;
 mod app;
 
 pub use frame::Frame;
+pub use time::Time;
 pub use app::App;
+
+/// run the app, hyjacking the main thread until the
+/// window is closed
+pub fn run<T: App + Default>()
+{
+    run_instance(T::default());
+}
+
+/// run the app, hyjacking the main thread until the
+/// window is closed
+///
+/// takes an instance of an app; use [run] if your app type
+/// implements [Default]
+///
+/// [run]: crate::run
+/// [Default]: Default
+pub fn run_instance<T: App>(mut state: T)
+{
+    use pixels::{ Pixels, SurfaceTexture };
+
+    use winit::dpi::LogicalSize;
+    use winit::event::Event;
+    use winit::event_loop::{ControlFlow, EventLoop};
+    use winit::window::WindowBuilder;
+    use winit_input_helper::WinitInputHelper;
+
+    let size = LogicalSize::new(T::WIDTH as f64, T::HEIGHT as f64);
+
+    let events = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_min_inner_size(size)
+        .with_inner_size(size)
+        .with_title(T::TITLE)
+        .build(&events)
+        .unwrap();
+
+    let mut pixels =
+    {
+        let w_size = window.inner_size();
+        let surf_tex = SurfaceTexture::new(w_size.width, w_size.height, &window);
+        
+        Pixels::new(T::WIDTH as u32, T::HEIGHT as u32, surf_tex).unwrap()
+    };
+
+    let mut input = WinitInputHelper::new();
+    //let mut time = Instant::now();
+    //let mut dt = 0.0;
+    
+    events.run(move |evt, _, control_flow|
+    {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = evt
+        {
+            // get the frame
+            let frame = Frame::new(pixels.get_frame(), T::WIDTH, T::HEIGHT);
+            
+            // update buffer
+            state.render(frame);
+
+            // render
+            if pixels.render().is_err()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // handle input events
+        if input.update(&evt)
+        {
+            // close events
+            if input.quit()
+            {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // resize the window
+            if let Some(size) = input.window_resized()
+            {
+                pixels.resize(size.width, size.height);
+            }
+
+            // Update the FPS
+            //dt = time.elapsed().as_secs_f64();
+
+            // update state
+            state.update();
+            
+            // request a redraw
+            window.request_redraw();
+
+            // reset delta time
+            //time = Instant::now();
+        }
+    });
+}
