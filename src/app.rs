2d164b2db64bb7336ebd1978b4cd4555a392c7c8
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2020-12-14 14:07:11 -0800

    better lighting

--- src/app.rs
+++ src/app.rs
@@ -6,6 +6,7 @@ use crate::draw;
 
 pub struct MyApp
 {
+    light: Vec3<f32>,
     obj: Obj,
 }
 
@@ -28,7 +29,7 @@ impl App for MyApp
         let size: Vec2<f32> = Self::SIZE.as_().into();
 
         // lighting
-        let light_dir = Vec3::new(1.0, 0.45, 2.0).normalized();
+        let light_dir = self.light.normalized();
 
         // iterate object's triangle faces
         for [mut v0, mut v1, mut v2] in self.obj.iter_faces()
@@ -44,7 +45,7 @@ impl App for MyApp
 
             // lighting
             let n = (v2 - v0).cross(v1 - v0).normalized();
-            let l = n.dot(light_dir);
+            let l = (n.dot(light_dir)).clamped_minus1_1().powi(2);
 
             // visible face
             if l > 0.0
@@ -55,21 +56,25 @@ impl App for MyApp
                 // draw mesh
                 draw::triangle(frame, &mut depth, [v0, v1, v2], col);
 
-                // draw wireframe
+                // // prepare wireframe
+                // let pts = [v0.xy().as_(), v1.xy().as_(), v2.xy().as_()];
+                // let wht = Rgba::white();
+
+                // // draw wireframe
                 // draw::line(frame, pts[0], pts[1], wht);
                 // draw::line(frame, pts[0], pts[2], wht);
                 // draw::line(frame, pts[1], pts[2], wht);
             }
-            // convert
-            // let pts = [v0.xy().as_(), v1.xy().as_(), v2.xy().as_()];
-            // let col = [l, l, l, 0xff].into();
-            // let wht = Rgba::white();
         }
     }
 
     fn update(&mut self, time: &Time)
     {
         println!("FPS: {:.1}", 1.0 / time.dt());
+
+        let (s, c) = time.elapsed().as_secs_f32().sin_cos();
+
+        self.light = Vec3::new(c, s, c * s);
     }
 }
 
@@ -79,6 +84,7 @@ impl Default for MyApp
     {
         Self
         {
+            light: Vec3::zero(),
             obj: Obj::load("res/head.obj"),
         }
     }
